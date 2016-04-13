1a1d3e2360832beeac4b3adc4127acd7645c4d98
Patrick Ryan <pjryan126@gmail.com>
2016-04-13 15:45:33 -0400

    update batch_load file

--- store/batch/batch_load.sh
+++ store/batch/batch_load.sh
@@ -1,14 +1,14 @@
 # create a folder for each data set
-hdfs dfs ‐mkdir /user/w205/src/crime_by_state
-hdfs dfs ‐mkdir /user/w205/src/median_earnings
-hdfs dfs ‐mkdir /user/w205/src/park_score
-hdfs dfs ‐mkdir /user/w205/src/quality_of_life
+sudo -u w205 hdfs dfs ‐mkdir /user/w205/src/crime_by_state
+sudo -u w205 hdfs dfs ‐mkdir /user/w205/src/median_earnings
+sudo -u w205 hdfs dfs ‐mkdir /user/w205/src/park_score
+sudo -u w205 hdfs dfs ‐mkdir /user/w205/src/quality_of_life
 
 # load source files into hdfs for long-term storage
-hdfs dfs -put crime_by_state.csv /user/w205/src/crime_by_state
-hdfs dfs ‐put median_earnings.csv /user/w205/src/median_earnings
-hdfs dfs ‐put park_score.csv /user/w205/src/park_score
-hdfs dfs ‐put quality_of_life.csv /user/w205/src/quality_of_life
+sudo -u w205 hdfs dfs -put crime_by_state.csv /user/w205/src/crime_by_state
+sudo -u w205 hdfs dfs ‐put median_earnings.csv /user/w205/src/median_earnings
+sudo -u w205 hdfs dfs ‐put park_score.csv /user/w205/src/park_score
+sudo -u w205 hdfs dfs ‐put quality_of_life.csv /user/w205/src/quality_of_life
 
 # load source files into hive for accessible storage
 hive -f ./hive_batch_ddl.hql
